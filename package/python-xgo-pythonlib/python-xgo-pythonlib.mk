7aee6406f37942df1fb808803da6d68dd8e8e463
Alexey Roslyakov <alexey.roslyakov@gmail.com>
2024-02-08 17:23:28 +0800

    Bump xgo-pythonlib to 0.3.2

--- package/python-xgo-pythonlib/python-xgo-pythonlib.mk
+++ package/python-xgo-pythonlib/python-xgo-pythonlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_XGO_PYTHONLIB_VERSION = 0.3.1
+PYTHON_XGO_PYTHONLIB_VERSION = 0.3.2
 PYTHON_XGO_PYTHONLIB_SOURCE = xgo-pythonlib-$(PYTHON_XGO_PYTHONLIB_VERSION).tar.gz
-PYTHON_XGO_PYTHONLIB_SITE = https://files.pythonhosted.org/packages/3c/08/1de416c3c770d4debce56d73a679cee8c1a57522922543381543e6cf0721
+PYTHON_XGO_PYTHONLIB_SITE = https://files.pythonhosted.org/packages/c5/f4/872b56153abe06792611f38815d059ee6e2bf3466a09e7a3e7018b8a73ec
 PYTHON_XGO_PYTHONLIB_SETUP_TYPE = setuptools
 PYTHON_XGO_PYTHONLIB_LICENSE = MIT
 
